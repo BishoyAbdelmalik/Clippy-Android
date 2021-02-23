65b7c1ccbf3ae578b27500d7d4567868cb6b3915
mrboushy <15853596+BishoyAbdelmalik@users.noreply.github.com>
2021-02-23 14:21:02 -0800

    download directly to clippy folder

--- app/src/main/java/apps/vip/clippy/downloadFile.java
+++ app/src/main/java/apps/vip/clippy/downloadFile.java
@@ -31,75 +31,29 @@ public class downloadFile {
         Uri uri = Uri.parse(url);
 
         DownloadManager.Request request = new DownloadManager.Request(uri);
-        request.setTitle("My File");
-        request.setDescription("Downloading");
-        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE);
+        request.setTitle("File Received: "+filename);
+        request.setDescription("downloading...");
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
         request.setVisibleInDownloadsUi(false);
+        //create output directory if it doesn't exist
+        File dir = new File("/storage/emulated/0/clippy/");
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        File file = new File(dir, filename);
+        request.setDestinationUri(Uri.fromFile(file));
 
-        File directory = new File(context.getExternalFilesDir(null), filename);
-        request.setDestinationUri(Uri.fromFile(directory));
-
-        System.out.println("path " + Uri.fromFile(directory));
+        System.out.println("path " + Uri.fromFile(file));
 
 
         downloadmanager.enqueue(request);
         BroadcastReceiver onComplete = new BroadcastReceiver() {
             public void onReceive(Context ctxt, Intent intent) {
-                // your code
-                moveFile(directory.getParentFile().getAbsolutePath()+"/", filename, "/storage/emulated/0/clippy/");
-
                 Toast.makeText(context, "File Received", Toast.LENGTH_SHORT).show();
-//                NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
-//                        .setSmallIcon(R.drawable.ic_launcher_foreground)
-//                        .setContentTitle("File Received")
-//                        .setContentText(filename)
-//                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-//                builder.notify();
-
             }
         };
         context.registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
 
     }
 
-    private void moveFile(String inputPath, String inputFile, String outputPath) {
-
-        InputStream in = null;
-        OutputStream out = null;
-        try {
-
-            //create output directory if it doesn't exist
-            File dir = new File(outputPath);
-            if (!dir.exists()) {
-                dir.mkdirs();
-            }
-
-
-            in = new FileInputStream(inputPath + inputFile);
-            out = new FileOutputStream(outputPath + inputFile);
-
-            byte[] buffer = new byte[1024];
-            int read;
-            while ((read = in.read(buffer)) != -1) {
-                out.write(buffer, 0, read);
-            }
-            in.close();
-            in = null;
-
-            // write the output file
-            out.flush();
-            out.close();
-            out = null;
-
-            // delete the original file
-            new File(inputPath + inputFile).delete();
-
-
-        } catch (FileNotFoundException fnfe1) {
-            Log.e("tag", fnfe1.getMessage());
-        } catch (Exception e) {
-            Log.e("tag", e.getMessage());
-        }
-
-    }
 }
