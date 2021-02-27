3f8280f559c623790d67cc129abd9c9c934b2e86
mrboushy <15853596+BishoyAbdelmalik@users.noreply.github.com>
2021-02-26 17:13:05 -0800

    added links notification

---
+++ app/src/main/java/apps/vip/clippy/LinksPage.java
@@ -0,0 +1,14 @@
+package apps.vip.clippy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LinksPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_links_page);
+    }
+}
