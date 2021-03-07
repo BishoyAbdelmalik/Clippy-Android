da03e0c7593a449fc56ffa23dd3e6d33340b7b99
mrboushy <15853596+BishoyAbdelmalik@users.noreply.github.com>
2021-03-07 13:34:01 -0800

    remove unused import

--- app/src/main/java/apps/vip/clippy/MainActivity.java
+++ app/src/main/java/apps/vip/clippy/MainActivity.java
@@ -2,19 +2,13 @@ package apps.vip.clippy;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
+
 
 import android.Manifest;
 import android.app.Activity;
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
@@ -24,7 +18,6 @@ import android.widget.Toast;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     String ip="192.168.0.40";
