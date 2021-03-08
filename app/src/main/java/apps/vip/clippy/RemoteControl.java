0e061a47d5c60bed6873362faaa668ca3bb3d8e2
mrboushy <15853596+BishoyAbdelmalik@users.noreply.github.com>
2021-03-08 13:19:48 -0800

    added page

---
+++ app/src/main/java/apps/vip/clippy/RemoteControl.java
@@ -0,0 +1,14 @@
+package apps.vip.clippy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RemoteControl extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remote_control);
+    }
+}
--- app/src/main/java/apps/vip/clippy/main_page.java
+++ app/src/main/java/apps/vip/clippy/main_page.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -47,6 +48,8 @@ public class main_page extends AppCompatActivity {
             CustomTabsIntent customTabsIntent = builder.build();
             customTabsIntent.launchUrl(context, Uri.parse(url));
         });
+        Button remoteControlPage=findViewById(R.id.remote_control_page);
+        remoteControlPage.setOnClickListener(v -> startActivity(new Intent(context,RemoteControl.class)));
 
 
     }
