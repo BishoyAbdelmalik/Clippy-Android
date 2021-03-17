e1f43aa3ed1169b0c082c71c4d88892b3d924c81
Arian-D <arianxdehghani@gmail.com>
2021-03-17 02:01:31 -0800

    Added a hard-coded solution to get the thumbnail to show up

--- app/src/main/java/apps/vip/clippy/Connection.java
+++ app/src/main/java/apps/vip/clippy/Connection.java
@@ -63,6 +63,7 @@ public class Connection {
                                     media_control.playingTxt.setText(title);
                                 }
                                 media_control.playingTXT = title;
+                                media_control.thumbnailUrl = "http://192.168.0.19:5000" + thumb;
                                 System.out.println("playing title from connection " + title);
                             }
                         }
--- app/src/main/java/apps/vip/clippy/media_control.java
+++ app/src/main/java/apps/vip/clippy/media_control.java
@@ -2,21 +2,43 @@ package apps.vip.clippy;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.ClipboardManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.net.URL;
+
 
 public class media_control extends AppCompatActivity {
-    public static TextView playingTxt = null;
     public static ImageView playingThumb = null;
+    public static String thumbnailUrl = "https://memesr.com/meme-templates/doge-meme.png";
+    public static TextView playingTxt = null;
     public static String playingTXT = "Nothing Playing";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_media_control);
         playingThumb = findViewById(R.id.playingThumb);
+        try {
+            URL url = new URL(thumbnailUrl);
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try  {
+                        Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+                        playingThumb.setImageBitmap(bmp);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            thread.start();
+        } catch (Exception e) {
+            System.err.println("lmao get fukd user");
+        }
         playingTxt = findViewById(R.id.playing);
         playingTxt.setText(playingTXT);
         findViewById(R.id.playPause).setOnClickListener(v -> ForegroundService.sendCommand((ClipboardManager) getSystemService(CLIPBOARD_SERVICE), "playPause"));
